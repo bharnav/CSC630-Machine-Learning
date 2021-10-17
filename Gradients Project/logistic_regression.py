e1c570f129c3f5e49777e302a629e1e81f0bf39d
bharnav <55301041+bharnav@users.noreply.github.com>
2021-10-17 14:35:18 -0400

    updated logistic_regression.py

--- Gradients Project/logistic_regression.py
+++ Gradients Project/logistic_regression.py
@@ -1,45 +1,58 @@
 import numpy as np
 import math
 from variable import Variable
+
 class LogisticRegression():
-    def __init__(self, learning_rate = 0.01, epochs = 100, verbose = True):
+    def __init__(self, learning_rate = 0.001, epochs = 50, verbose = False):
         self.lr = learning_rate
         self.verbose = verbose
-        self.iters = epochs
+        self.epochs = epochs
         pass
+    
+    def sigmoid(self, w, val, b, dims):
+        act_func = []
+        for i in range(dims):
+            lin_comb = sum(c * x for c, x in zip(w, val[i]))
+            act_func.append((1 / (1 + Variable.exp((lin_comb) + b))))
+        return act_func
+    
+    def loss_function(self, pred, true, dims):
+        neg_avg = ((0 - 1) / dims)
+        return neg_avg * sum([true[i] * Variable.log(pred[i]) + (1 - true[i]) * Variable.log(1 - pred[i]) for i in range(dims)])
 
-    def fit(self, X, y, threshold):
-        m, n = np.shape(X)
-        w = [Variable(name = f"w_{i}") for i in range(n)]
-        b = Variable(name='b')
-        yh = [(1/(1 + Variable.exp(sum(np.array(w) * X[i]) + b))) for i in range(m)]
-        cost = (0 - 1) * sum([y[i] * Variable.log(yh[i]) + (1 - y[i]) * Variable.log(1 - yh[i]) for i in range(m)])
-
+    def fit(self, X, y):
+        m = np.shape(X)[0]
+        n = np.shape(X)[1]
+        w = [Variable(name = i) for i in range(n)]
+        b = Variable(name = "bias")
+        h = self.sigmoid(w, X, b, m)
+        cost = self.loss_function(h, y, m)
         self.w_iter = np.zeros(n)
         self.b_iter = 0
         
         cost_list = []
-        for wq in range(self.iters):
-            dict_values = {'b':self.b_iter}
-            dict_values.update({f'w_{i}':self.w_iter[i] for i in range(n)})
-
-            step = cost.gradient(dict_values)
-
-            self.b_iter -= self.lr * cost.gradient(dict_values)[0]
-            self.w_iter -= self.lr * cost.gradient(dict_values)[1:]
-
-            loss = cost.evaluate(dict_values)
-            
+        for wq in range(self.epochs):
+            all_vars_coeffs = {i : self.w_iter[i] for i in range(n)}
+            all_vars_coeffs.update({"bias" : self.b_iter})
+            self.w_iter = self.w_iter - self.lr * cost.gradient(all_vars_coeffs)[0 : len(cost.gradient(all_vars_coeffs)) - 1]
+            self.b_iter = self.b_iter - self.lr * cost.gradient(all_vars_coeffs)[-1]
+            cost_list.append(cost.evaluate(all_vars_coeffs))
             if self.verbose:
-                print("cost after ", wq, "iterations is - ", loss)
-        print(f"model summary: cost - {loss}", f"weights - {self.w_iter}",f"bias - {self.b_iter}")
-
+                if wq <= 9:
+                    print(f"[{wq}]     ", "loss:", cost_list[wq])
+                elif wq <= 99:
+                    print(f"[{wq}]    ", "loss:", cost_list[wq])
+                elif wq <= 999:
+                    print(f"[{wq}]   ", "loss:", cost_list[wq])
+                elif wq <= 9999:
+                    print(f"[{wq}]  ", "loss:", cost_list[wq])
+                else:
+                    print(f"[{wq}] ", "loss:", cost_list[wq])
+        print(f"LogisticRegressor(loss_score={cost_list[self.epochs-1]},", f"weights={self.w_iter},", f"bias={self.b_iter})")
 
     def predict(self, X):
-        m, n = np.shape(X)
-        predictions = []
-
-        for i in range(m):
-            predictions.append(1/(1+math.exp(sum(np.array(self.w_iter)*dataset[i]) + self.b_iter)))
-
-        return predictions
+        vals = []
+        for i in range(np.shape(X)[0]):
+            lin_comb = sum(c * x for c, x in zip(self.w_iter, X[i]))
+            vals.append((1 / (1 + math.exp((lin_comb) + self.b_iter))))
+        return vals
